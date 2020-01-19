4686c6232eaa70e90acd76e6dbcb6744c2e5b2c9
Mohit Anand <mohitanand0810@gmail.com>
2020-01-19 13:48:34 +0530

    Current State of the game

--- Game_2048.py
+++ Game_2048.py
@@ -15,3 +15,29 @@ def add_new_2(mat):
         r = random.randint(0, 3)
         c = random.randint(0, 3)
     mat[r][c] = 2
+
+# step - 3 : Current state of the game
+def get_current_state(mat):
+    # Anywhere 2048 is present
+    for i in range(4):
+        for j in range(4):
+            if mat[i][j] == 2048:
+                return "Congratulations !! You Won."
+    # Anywhere 0 is present
+    for i in range(4):
+        for j in range(4):
+            if (mat[i][j] == 0):
+                return "Game Not Over !!"
+    # Every Row and column except last row and last column
+    for i in range(3):
+        for j in range(3):
+            if (mat[i][j] == mat[i+1][j] or mat[i][j] == mat[i][j+1] ):
+                return "Game Not Over !!"
+    # Last Row
+    for j in range(3):
+        if mat[3][j] == mat[3][j+1]:
+            return "Game Not Over !!"
+    # Last Column
+    for i in range(3):
+        if mat[i][3] == mat[i+1][3]:
+            return "Game Not Over !!"
