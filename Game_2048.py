7384db609a41239b94c3c10be6be9051449334a7
Mohit Anand <mohitanand0810@gmail.com>
2020-01-19 14:01:15 +0530

    Merge

--- Game_2048.py
+++ Game_2048.py
@@ -54,3 +54,11 @@ def compress(mat):
                 new_mat[i][pos] = mat[i][j]
                 pos += 1
     return new_mat
+
+# step - 5 : Merge
+def merge(mat):
+    for i in range(4):
+        for j in range(3):
+            if mat[i][j] == mat[i][j+1] and mat[i][j] != 0:
+                mat[i][j] = mat[i][j]*2
+                mat[i][j+1] = 0
